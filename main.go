e14d7d8f8d043a3eac62971067806ad93ef364d5
Sandesh-Siddhewar <Sandeshsiddhewar@gmail.com>
2025-12-11 13:07:41 +0530

    person by id

--- main.go
+++ main.go
@@ -7,24 +7,40 @@ import (
 )
 
 type Person struct {
-	Id      int64  `json:"id"`
+	Id      string `json:"id"`
 	Name    string `json:"name"`
 	Address string `json:"address"`
 }
 
 var person = []Person{
-	{1, "sandesh", "Belapur CBD"},
-	{2, "Vishal", "At. Post Ulwe"},
+	{"1", "sandesh", "Belapur CBD"},
+	{"2", "Vishal", "At. Post Ulwe"},
 }
 
-func getPerson(c *gin.Context) {
+func getPersons(c *gin.Context) {
 	c.JSON(http.StatusOK, person)
 
 }
 
+func getPerson(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, person := range person {
+		if person.Id == id {
+			c.JSON(http.StatusOK, person)
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"Message": "Person not found"})
+}
+
+func createPerson(c *gin.Context) {
+
+}
 func main() {
 	router := gin.Default()
 
-	router.GET("/person", getPerson)
-	router.Run(":8080")
+	router.GET("/persons", getPersons)
+	router.GET("/person/:id", getPerson)
+	router.Run(":8081")
 }
